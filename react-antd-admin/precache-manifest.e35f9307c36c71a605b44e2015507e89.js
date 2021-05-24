924fa3673790961b36009cfb2d360464d7ecb843
chenjun <chenjun@orvibo.com>
2021-05-24 13:47:43 +0800

    update

---
+++ react-antd-admin/precache-manifest.e35f9307c36c71a605b44e2015507e89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45b81edb2bed4aa307151a6e9d6e9cd2",
+    "url": ".//index.html"
+  },
+  {
+    "revision": "e4d7674d2c4688127848",
+    "url": ".//static/css/2.a5b3448b.chunk.css"
+  },
+  {
+    "revision": "7d49805581fbe04a128c",
+    "url": ".//static/css/main.157aee77.chunk.css"
+  },
+  {
+    "revision": "e4d7674d2c4688127848",
+    "url": ".//static/js/2.1b79e475.chunk.js"
+  },
+  {
+    "revision": "7d49805581fbe04a128c",
+    "url": ".//static/js/main.328ec9e8.chunk.js"
+  },
+  {
+    "revision": "c61a4d9c77ffbfd06ccb",
+    "url": ".//static/js/runtime~main.55b4d891.js"
+  }
+]);
